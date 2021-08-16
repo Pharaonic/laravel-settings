73b343eaf1078fd0f66cbd8e63c8be0d58604c19
Moamen Eltouny <support@raggitech.com>
2021-08-16 22:48:04 +0200

    add Settings Object, Settingable Trait

--- src/Classes/Settings.php
+++ src/Classes/Settings.php
@@ -2,6 +2,135 @@
 
 namespace Pharaonic\Laravel\Settings\Classes;
 
-final class Settings {
-    
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Pharaonic\Laravel\Settings\Models\Settings as ModelsSettings;
+
+/**
+ * @property Model|null $model
+ * @property Collection $data
+ *
+ * @author Moamen Eltouny (Raggi) <raggi@raggitech.com>
+ */
+final class Settings
+{
+    /**
+     * Model Object.
+     *
+     * @var Model|null
+     */
+    private $model;
+
+    /**
+     * Settings data list.
+     *
+     * @var Collection|null
+     */
+    private $data;
+
+    /**
+     * Create a new instance.
+     *
+     * @param Model|null $model
+     * @param Collection|null $data
+     */
+    public function __construct(Model $model = null, Collection $data = null)
+    {
+        if ($model) {
+            $this->model = $model;
+            $this->data = $data;
+        } else {
+            $this->data = ModelsSettings::whereNull('modelable_type')->get()->keyBy('name');
+        }
+    }
+
+    /**
+     * Getting record value
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        if (!$this->data[$name]) return;
+
+        return $this->data[$name]->value;
+    }
+
+    /**
+     * Setting key/value information
+     *
+     * @param string $name
+     * @param mixed $value
+     */
+    public function __set(string $name, $value = null)
+    {
+        $this->set($name, $value);
+    }
+
+    /**
+     * Getting record value
+     *
+     * @param mixed ...$names
+     * @return Collection
+     */
+    public function get(...$names)
+    {
+        if (empty($names)) return [];
+        if (is_array($names[0])) $names = $names[0];
+
+        $data = $this->data->map(function ($value, $name) use ($names) {
+            if (in_array($name, $names)) return $value;
+            return null;
+        })->filter(function ($value) {
+            return !is_null($value);
+        });
+
+        return count($names) == 1 ? $data->first() : $data;
+    }
+
+    /**
+     * Setting key/value information
+     *
+     * @param mixed ...$values
+     * @return void
+     */
+    public function set(...$values)
+    {
+        if (empty($values)) return;
+
+        if (is_array($values[0])) {
+            // Multi
+            foreach ($values[0] as $name => $value)
+                $this->set($name, $value);
+        } elseif (is_string($values[0])) {
+            // Single
+            if ($this->data->has($values[0])) {
+                $this->data[$values[0]]->value = $values[1] ?? null;
+            } else {
+                $obj = new ModelsSettings;
+                $obj->name  = $values[0];
+                $obj->value = $values[1] ?? null;
+
+                if ($this->model) {
+                    $obj->modelable_type    = get_class($this->model);
+                    $obj->modelable_id      = $this->model->getKey();
+                }
+
+                $this->data->put($values[0], $obj);
+            }
+        }
+    }
+
+    /**
+     * Save all the dirty objects.
+     *
+     * @return void
+     */
+    public function save()
+    {
+        foreach ($this->data as $obj)
+            if ($obj->isDirty())
+                $obj->save();
+    }
 }
--- src/Traits/Settingable.php
+++ src/Traits/Settingable.php
@@ -2,7 +2,72 @@
 
 namespace Pharaonic\Laravel\Settings\Traits;
 
+use Illuminate\Database\Eloquent\Model;
+use Pharaonic\Laravel\Settings\Classes\Settings as ClassesSettings;
+use Pharaonic\Laravel\Settings\Models\Settings;
+
+/**
+ * Settingable any model.
+ *
+ * @property \Illuminate\Support\Collection|null $settings
+ *
+ * @author Moamen Eltouny (Raggi) <raggi@raggitech.com>
+ */
 trait Settingable
 {
-    
+    /**
+     * Settings records list.
+     *
+     * @var \Pharaonic\Laravel\Settings\Classes\Settings
+     */
+    public $settings_data;
+
+    /**
+     * Boot Settingable (saved)
+     *
+     * @return void
+     */
+    public static function bootSettingable()
+    {
+        // STORE/UPDATE
+        static::saved(function (Model $model) {
+            $model->settings->save();
+        });
+
+        // DESTROY
+        static::deleting(function (Model $model) {
+            $model->clearSettings();
+        });
+    }
+
+    /**
+     * Get the user settings.
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    private function getSettingsColelction()
+    {
+        return $this->morphMany(Settings::class, 'modelable');
+    }
+
+    /**
+     * Getting Settings Object
+     *
+     * @return ClassesSettings
+     */
+    public function getSettingsAttribute()
+    {
+        if ($this->settings_data) return $this->settings_data;
+
+        return $this->settings_data = new ClassesSettings($this, $this->getSettingsColelction()->get()->keyBy('name'));
+    }
+
+    /**
+     * Clear all settings
+     *
+     * @return boolean
+     */
+    public function clearSettings()
+    {
+        return $this->getSettingsColelction()->delete() > 0;
+    }
 }
