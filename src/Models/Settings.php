25044c2918bad5d478f2e3d1805c9bbb195de523
Moamen Eltouny <16774083+MoamenEltouny@users.noreply.github.com>
2022-05-06 06:02:45 +0200

    add accessor & mutator for settings model

--- src/Models/Settings.php
+++ src/Models/Settings.php
@@ -23,4 +23,33 @@ class Settings extends Model
      * @var array
      */
     protected $fillable = ['name', 'value', 'modelable_type', 'modelable_id'];
+
+    /**
+     * Getting string/array value
+     *
+     * @return string|array|null
+     */
+    protected function getValueAttribute()
+    {
+        $value = $this->getAttributes()['value'];
+
+        if ($value && substr($value, 0, 1) == '{')
+            return json_decode($value);
+
+        return $value;
+    }
+
+    /**
+     * Setting string/array value
+     * 
+     * @param string|array|null $value
+     * @return void
+     */
+    protected function setValueAttribute($value)
+    {
+        if (is_array($value))
+            $value = json_encode($value);
+
+        $this->attributes['value'] = $value;
+    }
 }
